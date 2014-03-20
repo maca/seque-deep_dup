7f9c5f2c8af6da3450d7808bb01f4ec7e3916e45
macario <mail@makarius.me>
2014-03-20 12:58:32 -0600

    basic assoc cloning

--- lib/sequel/plugins/deep_dup.rb
+++ lib/sequel/plugins/deep_dup.rb
@@ -2,46 +2,65 @@ module Sequel
   module Plugins
     module DeepDup
       class << self
-        def apply model, opts=OPTS
-          # model.instance_variable_set :@touched_associations, {}
-        end
-
-        def configure model, opts=OPTS
-          # model.touch_associations(opts[:associations]) if opts[:associations]
-        end
-
-        def deep_dup instance, opts = {}
-          associations = opts[:associations] || []
-
-          values = instance.to_hash.dup
-          instance.pk_hash.keys.each{ |attr| values.delete(attr) }
-          copy = instance.class.create values
-
-          associations.map do |name|
-            next unless refl = instance.class.association_reflection(name)
-            associated = instance.send(name)
-            
-            case refl[:type]
-            when :one_to_many
-              associated.each do |rec|
-                new_rec = deep_dup(rec)
-                copy.send refl.add_method, new_rec
-              end
-            end
-          end
-
-          copy
+        def apply model
+          model.plugin(:instance_hooks)
         end
       end
 
       module ClassMethods
-        # def touch_associations *associations
-        # end
+      end
+
+      class DeepDupper
+        attr_reader :instance
+
+        def initialize instance
+          @instance = instance
+        end
+
+        def shallow_dup instance
+          klass      = instance.class
+          attributes = instance.to_hash.dup
+          [*klass.primary_key].each{ |attr| attributes.delete(attr) } 
+          klass.new attributes
+        end
+
+        def dup_associations instance, copy, associations
+          associations.each do |name|
+            next unless refl = instance.class.association_reflection(name)
+            [*instance.send(name)].each { |rec| instantiate_associated(copy, refl, rec) }
+          end
+        end
+
+        def instantiate_associated copy, reflection, record
+          record = shallow_dup(record)
+
+          if reflection.returns_array?
+            copy.send(reflection[:name]) << record
+            copy.after_save_hook{ copy.send(reflection.add_method, record) }
+          end
+          
+
+          # else
+          #   # case refl[:type]
+          #   # when :one_to_many
+          #   #   associated.each do |rec|
+          #   #     new_rec = deep_dup(rec)
+          #   #     copy.send refl.add_method, new_rec
+          #   #   end
+          #   # end
+          #   end
+        end
+
+        def dup
+          copy = shallow_dup instance
+          dup_associations instance, copy, instance.class.associations
+          copy
+        end
       end
 
       module InstanceMethods
         def deep_dup
-          DeepDup.deep_dup self, associations: self.class.associations
+          DeepDupper.new(self).dup
         end
       end
     end
--- spec/deep_dup_spec.rb
+++ spec/deep_dup_spec.rb
@@ -6,47 +6,30 @@ describe Sequel::Plugins::DeepDup do
   let(:course)          { create :course, name: 'Ruby', starts: Date.today, ends: Date.today + 30 }
   let(:course_copy)     { course.deep_dup }
   let(:student)         { create :student }
-  let(:enrollment)      { create :enrollment, student: student, course: course }
+  let(:enrollment)      { create :enrollment }
   let(:enrollment_copy) { enrollment.deep_dup }
 
-  describe 'attribute duplication' do
+  describe 'cloning a plain record' do
     before do
-      Course.plugin :deep_dup
+      Program.plugin :deep_dup
       Enrollment.plugin :deep_dup
     end
 
-    it 'clones attributes' do
-      course_copy.name.should   == 'Ruby'
-      course_copy.starts.should == Date.today
-      course_copy.ends.should   == Date.today + 30
-    end
-    
-    it 'sets a new primary key for the copy' do
-      course_copy.id.should_not == course.id
-    end
-
-    it 'sets a new composite primary key for copy' do
-      enrollment_copy.pk.should_not == enrollment.id
-    end
+    it { program_copy.name.should == 'CS' }
+    it { program_copy.pk.should be_nil } 
+    it { program_copy.should be_new } 
+    it { enrollment_copy.pk.should == [nil, nil] } 
+    it { enrollment_copy.should be_new }
   end
 
-  describe 'association duplication' do
-    let(:courses) { 3.times.map { |num| attributes_for :course, name: "Course #{num}" } }
-
+  describe 'clonig a record with a one to many association' do
     before do
       Program.plugin :deep_dup
-      courses.each { |course| program.add_course course }
+      3.times { |num| program.add_course name: "Course #{num}" }
     end
 
-    it { expect { program_copy }.to change { Course.count }.by(3) }
     it { program_copy.should have(3).courses }
-
-    it 'has different courses for copy than for the original' do
-      orig_course_ids = program.courses.map(&:id) 
-      copy_course_ids = program_copy.courses.map(&:id)
-      all_course_ids  = orig_course_ids | copy_course_ids
-
-      all_course_ids.should have(6).items
-    end
+    it { program_copy.courses.map(&:pk).should be_none }
+    it { expect { program_copy.save }.to change{ Course.count }.by(3) }
   end
 end
